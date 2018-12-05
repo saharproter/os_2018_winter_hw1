f71e6f42134b7fa29c87909ad3f273819712a201
saharproter <35033961+saharproter@users.noreply.github.com>
2018-12-05 23:37:19 +0200

    Add files via upload

---
+++ hw2_syscalls.c
@@ -0,0 +1,65 @@
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include<stdio.h>
+
+
+int is_changeable(pid_t pid){
+    int res;
+    __asm__(
+    "int $0x80;"
+    : "=a" (res)
+    : "0" (243), "b" (pid)
+    :"memory"
+    );
+    if ((res) < 0) {
+        errno = (-res);
+        return -1;
+    }
+    return res;
+}
+
+int make_changeable(pid_t pid){
+    int res;
+    __asm__(
+    "int $0x80;"
+    : "=a" (res)
+    : "0" (244), "b" (pid)
+    :"memory"
+    );
+    if ((res) < 0) {
+        errno = (-res);
+        return -1;
+    }
+    return res;
+}
+
+int change(int val){
+    int res;
+    __asm__(
+    "int $0x80;"
+    : "=a" (res)
+    : "0" (245), "b" (val)
+    :"memory"
+    );
+    if ((res) < 0) {
+        errno = (-res);
+        return -1;
+    }
+    return res;
+}
+
+int is_changeable(pid_t pid){
+    int res;
+    __asm__(
+    "int $0x80;"
+    : "=a" (res)
+    : "0" (246), "b" (pid)
+    :"memory"
+    );
+    if ((res) < 0) {
+        errno = (-res);
+        return -1;
+    }
+    return res;
+}
